2251a004e31ceb2c08eecf94ea6c17d7117f17a3
Lucasrba10 <lucasrbazevedo_@hotmail.com>
2025-06-23 06:47:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ fat_percentage = st.number_input('Percentual de Gordura Corporal (%)', min_value
 weight = st.number_input('Peso (kg)', min_value=0.0, value=70.0)
 session_duration = st.number_input('Duração da Sessão (horas)', min_value=0.0, value=1.0)
 height = st.number_input('Altura (m)', min_value=0.0, value=1.70)
-avg_bpm = st.number_input('Média de BPM durante o treino', min_value=0, value=120.0)
+avg_bpm = st.number_input('Média de BPM durante o treino', min_value=0.0, value=120.0)
 age = st.number_input('Idade', min_value=0, value=25)
-water_intake = st.number_input('Consumo de Água (litros)', min_value=0, value=2.0)
-resting_bpm = st.number_input('BPM em repouso', min_value=0, value=70.0)
+water_intake = st.number_input('Consumo de Água (litros)', min_value=0.0, value=2.0)
+resting_bpm = st.number_input('BPM em repouso', min_value=0.0, value=70.0)
 
 # Botão para prever
 if st.button('Prever Calorias Queimadas'):
